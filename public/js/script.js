dea5e1b53206919a3498ec039249ca965dc90719
pentatonic <john.pentatonic@gmail.com>
2017-06-03 23:11:47 +0800

    Fix school page layout

--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,47 @@
 var channelId = 'UCfhr-95kSAejI58boC7j7FA'; //新北市政府都更小百科
 var myKey = 'AIzaSyAWvTTP8g5puqKLdeu5JqhUDDVrYGOWX4w';
-var vidWidth = 196;
-var vidHeight = 110;
-var vidResults = 50;
+var vidWidth = 560;//196;
+var vidHeight = 315;//110;
+var vidResults = 8;//50; // total 100
 
-$(document).ready(function (){
+$(document).ready(function () {
 	$.get(
 		"https://www.googleapis.com/youtube/v3/channels", {
 			part: 'contentDetails',
 			id: channelId,
-			key: myKey},
-			function(data) {
-				$.each(data.items, function(i, item) {
-					console.log(item);
-					pid = item.contentDetails.relatedPlaylists.uploads;
-					getVids(pid);
-				})
-			}
+			key: myKey
+		},
+		function (data) {
+			$.each(data.items, function (i, item) {
+				console.log(item);
+				pid = item.contentDetails.relatedPlaylists.uploads;
+				getVids(pid);
+			})
+		}
 	);
-	
+
 	function getVids(pid) {
 		$.get(
-		"https://www.googleapis.com/youtube/v3/playlistItems", {
-			part: 'snippet',
-			maxResults: vidResults,
-			playlistId: pid,
-			key: myKey},
-			function(data) {
+			"https://www.googleapis.com/youtube/v3/playlistItems", {
+				part: 'snippet',
+				maxResults: vidResults,
+				playlistId: pid,
+				key: myKey
+			},
+			function (data) {
 				var output;
-				$.each(data.items, function(i, item) {
+				$.each(data.items, function (i, item) {
 					console.log(item);
 					videoTitle = item.snippet.title;
 					videoId = item.snippet.resourceId.videoId;
-					
-					output = '<li><iframe height="'+vidHeight+'" width="'+vidWidth+'" src=\"//www.youtube.com/embed/'+videoId+'"></iframe></li>';
+
+					// output = '<li><iframe height="'+vidHeight+'" width="'+vidWidth+'" src=\"//www.youtube.com/embed/'+videoId+'" frameborder="0" allowfullscreen></iframe></li>';
+					output = '<iframe height="' + vidHeight + '" width="' + vidWidth + '" src=\"//www.youtube.com/embed/' + videoId
+						+ '" frameborder="0" allowfullscreen align="left" style="padding: 10px;"></iframe>';
 					//append to results
 					$('#results').append(output);
 				})
 			}
-	);
+		);
 	}
 });
